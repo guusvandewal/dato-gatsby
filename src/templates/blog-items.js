ae38f93e66ddd7e5d68ed9928eff67b515e39589
Guus van de Wal <info@guusvandewal.nl>
2022-05-25 16:56:25 +0200

    Show title

--- src/templates/blog-items.js
+++ src/templates/blog-items.js
@@ -1,12 +1,29 @@
-import * as React from "react"
+import React from "react"
+import { Link, graphql } from "gatsby"
 
+import Layout from "../components/layout"
 
-
-const Blog = ({props}) => (
-    console.log(props),
-        (
-            <div>Hi</div>
-        )
+const Blog = ({ data }) => (
+  <Layout>
+    <h1>Recipes</h1>
+    <ul>
+      {data.allDatoCmsBlog.edges.map(({ node }) => (
+        <li>{node.title}</li>
+      ))}
+    </ul>
+  </Layout>
 )
 
 export default Blog
+
+export const query = graphql`
+  query {
+    allDatoCmsBlog {
+      edges {
+        node {
+          title
+        }
+      }
+    }
+  }
+`
