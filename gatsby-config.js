440c5b3e698de971e3a0b339568c0554dd45dace
Guus van de Wal <info@guusvandewal.nl>
2022-06-05 14:33:58 +0200

    Up

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,12 +20,28 @@ module.exports = {
       options: {
         // You can find your read-only API token under the Settings > API tokens
         // section of your administrative area:
-        apiToken: 'b2de1ef33b592db5eb1568e20d0559',
+        apiToken: "b2de1ef33b592db5eb1568e20d0559",
         previewMode: false,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-sharp`,
+      options: {
+        defaults: {
+          formats: [`auto`, `webp`],
+          placeholder: `dominantColor`,
+          quality: 50,
+          breakpoints: [750, 1080, 1366, 1920],
+          backgroundColor: `transparent`,
+          tracedSVGOptions: {},
+          blurredOptions: {},
+          jpgOptions: {},
+          pngOptions: {},
+          webpOptions: {},
+          avifOptions: {},
+        },
+      },
+    },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
--- src/templates/blog-items.js
+++ src/templates/blog-items.js
@@ -1,48 +1,38 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
-import Markdown from 'markdown-to-jsx';
+import Markdown from "markdown-to-jsx"
 
 import Layout from "../components/layout"
-
-
-
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
 
 export default function Blog({ data }) {
-    console.log(data)
-    //const { frontmatter, html } = markdownRemark
-    return (
-        <Layout>
-            <h1>{data.datoCmsBlog.title}</h1>
-            <p>{data.datoCmsBlog.intro}</p>
-            <Markdown>{data.datoCmsBlog.intro}</Markdown>
-        </Layout>
-    )
+  console.log(data)
+  const query = data.datoCmsBlog
+  const image = getImage(query.media.gatsbyImageData)
+  return (
+    <Layout>
+      <h1>{query.title}</h1>
+      <Markdown>{query.intro}</Markdown>
+      <br />
+      <br />
+      <GatsbyImage image={image} alt={query.media.alt} />
+
+      {/* <div>{data.datoCmsBlog.media}</div>*/}
+    </Layout>
+  )
 }
-
-
-// TODO CLEANUP
-/*export const query = graphql`
-  query ($url: String!) {
-    allDatoCmsBlog(url: { eq: $url }) {
-      edges {
-        node {
-            id
-          title
-          url
-            intro
-        }
-      }
-    }
-  }
-`*/
 
 export const query = graphql`
   query MyQuery($id: String) {
-      datoCmsBlog(id: { eq: $id }) {
+    datoCmsBlog(id: { eq: $id }) {
       id
       title
       intro
       url
+      media {
+        gatsbyImageData
+        alt
+      }
     }
   }
 `
